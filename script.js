5b9e785034d1dd844bd155eff438128fcbcb70de
Nikhil Sharma <nikhil@192.168.1.3>
2023-07-17 12:38:16 +0530

    Improving the UI

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let container = document.querySelector('.container')
+let height = 400 / 16;
+let width = 400 / 16;
 for(let i = 0; i < 16; i++) {
     let rowDiv = document.createElement('div');
-    rowDiv.style.cssText = 'width: 256px; height: 16px;';
     rowDiv.classList.add('row-div');
     for(let j = 0; j < 16; j++) {
         let columnDiv = document.createElement('div');
         columnDiv.classList.add('column-div');
-        columnDiv.style.cssText = 'width: 16px; height: 16px; color: white';
-        columnDiv.textContent = "a";
+        columnDiv.style.height = height + 'px';
+        columnDiv.style.width = width + 'px';
         rowDiv.appendChild(columnDiv);
     }
     container.appendChild(rowDiv);
